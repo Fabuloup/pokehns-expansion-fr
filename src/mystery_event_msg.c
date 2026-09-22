eee98cb25eb1093857f2e2eb8cb8cc4f7437ddd7
NhyKoLa <nhykola@gmail.com>
2026-09-23 01:04:39 +0200

    FR: traduire mystery_event_msg

--- src/mystery_event_msg.c
+++ src/mystery_event_msg.c
@@ -1,13 +1,13 @@
 #include "global.h"
 
-const u8 gText_MysteryEventBerry[] = _("Obtained a {STR_VAR_2} BERRY!\nDad has it at PETALBURG GYM.");
-const u8 gText_MysteryEventBerryTransform[] = _("The {STR_VAR_1} BERRY transformed into\none {STR_VAR_2} BERRY.");
-const u8 gText_MysteryEventBerryObtained[] = _("The {STR_VAR_1} BERRY has already been\nobtained.");
-const u8 gText_MysteryEventSpecialRibbon[] = _("A special RIBBON was awarded to\nyour party POKéMON.");
-const u8 gText_MysteryEventNationalDex[] = _("The POKéDEX has been upgraded\nwith the NATIONAL MODE.");
-const u8 gText_MysteryEventRareWord[] = _("A rare word has been added.");
-const u8 gText_MysteryEventSentOver[] = _("{STR_VAR_1} was sent over!");
-const u8 gText_MysteryEventFullParty[] = _("Your party is full.\n{STR_VAR_1} could not be sent over.");
-const u8 gText_MysteryEventNewTrainer[] = _("A new TRAINER has arrived in\nHOENN.");
-const u8 gText_MysteryEventNewAdversaryInBattleTower[] = _("A new adversary has arrived in the\nBATTLE TOWER.");
-const u8 gText_MysteryEventCantBeUsed[] = _("This data can't be used in\nthis version.");
+const u8 gText_MysteryEventBerry[] = _("BAIE {STR_VAR_2} obtenue!\nPapa la garde à PETALBURG GYM.");
+const u8 gText_MysteryEventBerryTransform[] = _("La BAIE {STR_VAR_1} est devenue\nune BAIE {STR_VAR_2}.");
+const u8 gText_MysteryEventBerryObtained[] = _("La BAIE {STR_VAR_1} a déjà été\nobtenue.");
+const u8 gText_MysteryEventSpecialRibbon[] = _("Un RUBAN spécial a été remis\nà tes POKéMON.");
+const u8 gText_MysteryEventNationalDex[] = _("Le POKéDEX dispose maintenant\ndu MODE NATIONAL.");
+const u8 gText_MysteryEventRareWord[] = _("Un mot rare a été ajouté.");
+const u8 gText_MysteryEventSentOver[] = _("{STR_VAR_1} a été envoyé!");
+const u8 gText_MysteryEventFullParty[] = _("Ton équipe est complète.\nImpossible d'envoyer {STR_VAR_1}.");
+const u8 gText_MysteryEventNewTrainer[] = _("Un nouveau DRESSEUR est arrivé\nà HOENN.");
+const u8 gText_MysteryEventNewAdversaryInBattleTower[] = _("Un nouvel adversaire est arrivé\nà la TOUR DE COMBAT.");
+const u8 gText_MysteryEventCantBeUsed[] = _("Ces données sont inutilisables\ndans cette version.");
